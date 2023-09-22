735b1c916b895f182efff5259564d8fec0920fe3
Invarianz <invarianztheorem@web.de>
2023-09-22 21:51:02 +0200

    Some more import bugs in tests.

--- tests/30_Graphene_Nk1_60_Nk2_3_length_Gamma_M_dir_hexagonal_BZ/runscript.py
+++ tests/30_Graphene_Nk1_60_Nk2_3_length_Gamma_M_dir_hexagonal_BZ/runscript.py
@@ -1,11 +1,11 @@
 from params import params
 
-from cued.hamiltonian import Graphene
+from cued.hamiltonian.models import Graphene
 from cued.main import sbe_solver
 
 def model():
     t = 0.08                 #t coefficient
-    dft_system = cued.hamiltonian.Graphene(a=params.a, t=t)
+    dft_system = Graphene(a=params.a, t=t)
     return dft_system
 
 def run(system):
