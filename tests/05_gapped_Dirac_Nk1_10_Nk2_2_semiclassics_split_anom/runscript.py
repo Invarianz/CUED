74242f20cd24e25b14a440508268c7e08d409b73
Invarianz <invarianztheorem@web.de>
2023-09-22 21:41:41 +0200

    Changed indentation of python scripts.

--- tests/05_gapped_Dirac_Nk1_10_Nk2_2_semiclassics_split_anom/runscript.py
+++ tests/05_gapped_Dirac_Nk1_10_Nk2_2_semiclassics_split_anom/runscript.py
@@ -4,18 +4,18 @@ from cued.hamiltonian.models import BiTe
 from cued.main import sbe_solver
 
 def model():
-	A  = 0.1974      # Fermi velocity
-	mz = 0.01837     # prefactor of sigma_z in Hamiltonian
+    A  = 0.1974      # Fermi velocity
+    mz = 0.01837     # prefactor of sigma_z in Hamiltonian
 
-	dirac_system = cued.hamiltonian.BiTe(C0=0, C2=0, A=A, R=0, mz=mz)
+    dirac_system = BiTe(C0=0, C2=0, A=A, R=0, mz=mz)
 
-	return dirac_system
+    return dirac_system
 
 def run(system):
 
-	sbe_solver(system, params)
+    sbe_solver(system, params)
 
-	return 0
+    return 0
 
 if __name__ == "__main__":
-	run(model())
+    run(model())
--- tests/07_Dirac_Nk1_30_Nk2_2_length_Runge_Kutta_4/runscript.py
+++ tests/07_Dirac_Nk1_30_Nk2_2_length_Runge_Kutta_4/runscript.py
@@ -4,16 +4,16 @@ from cued.hamiltonian.models
 from cued.main import sbe_solver
 
 def model():
-	A = 0.1974      # Fermi velocity
+    A = 0.1974      # Fermi velocity
 
-	dirac_system = BiTe(C0=0, C2=0, A=A, R=0, mz=0)
+    dirac_system = BiTe(C0=0, C2=0, A=A, R=0, mz=0)
 
-	return dirac_system
+    return dirac_system
 def run(system):
 
-	sbe_solver(system, params)
+    sbe_solver(system, params)
 
-	return 0
+    return 0
 
 if __name__ == "__main__":
-	run(model())
+    run(model())
